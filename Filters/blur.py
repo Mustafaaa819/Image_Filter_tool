0b179633e55eb4311b2a7add9049e9087d448428
Mustafaaa819 <mustafakhan789987@gmail.com>
2025-07-28 08:32:55 +0500

    Added a basic Box-Blur filter Implementation

---
+++ Filters/blur.py
@@ -0,0 +1,47 @@
+import numpy as np
+from PIL import Image
+
+
+def blur_image(img_arr):
+    height = img_arr.shape[0]
+    width = img_arr.shape[1]
+    channels = img_arr.shape[2]
+
+    blurred = np.zeros_like(img_arr)
+
+    for row in range(1,height-1):
+        for col in range(1,width-1):
+            for c in range(channels):
+                #we make a 3x3 grid around pixel(y,x):
+                block = img_arr[row-1:row+2, col-1:col+2, c]
+
+                #then we take mean or average of all the numbers in grid:
+                averaged = np.mean(block)
+
+                #then we put this average value into the new image:
+                blurred[row,col,c] = averaged
+
+    return blurred
+
+img = Image.open('img.jpg')
+img_arr = np.array(img)
+
+blurred_arr = blur_image(img_arr)
+
+Image.fromarray(blurred_arr).save('blur.jpg')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from Filters.sepia import apply_sepia
 from Filters.darken import darken_image
 from Filters.invert import invert_image
 from Filters.black_white import black_and_white
+from Filters.blur import blur_image
 
 #Load the Image:
 image = Image.open('img.jpg')
@@ -16,22 +17,21 @@ image = Image.open('img.jpg')
 img_arr = np.array(image)
 
 #Apply Filters:
-gray_img = grayScaling_image(img_arr)
+gray_arr = grayScaling_image(img_arr)
 sepia_img = apply_sepia(img_arr)
 inverted_img = invert_image(img_arr)
 bright_img = apply_brightness(img_arr,1)
 dark_img = darken_image(img_arr)
 contrast_img = apply_contrast(img_arr, 1.1)
 bw_img = black_and_white(img_arr, 128)
+blur_the_image = blur_image(img_arr)
 
 #Save the Outputs:
-Image.fromarray(gray_img).save('gray.jpg')
+Image.fromarray(gray_arr).save('gray.jpg')
 sepia_img.save('sepia.jpg')
 inverted_img.save('inverted.jpg')
 bright_img.save('bright.jpg')
 dark_img.save('dark.jpg')
 contrast_img.save('contrast.jpg')
 bw_img.save('bw.jpg')
-
-
-
+blur_the_image.save('blur.jpg')
