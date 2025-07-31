b3a5001e1e5d62d8a89ccbfa6f9761303090beb8
Mustafaaa819 <mustafakhan789987@gmail.com>
2025-07-31 11:08:38 +0500

    Modified some features

--- Filters/sharpen.py
+++ Filters/sharpen.py
@@ -1,31 +1,63 @@
+# import numpy as np
+# from PIL import Image
+#
+# sharpening_kernel = [
+#     [-1,-1,-1],
+#     [-1,9,-1],
+#     [-1,-1,-1]
+# ]
+# sharpening_kernel = np.array(sharpening_kernel)
+#
+# def sharpen_image(image,kernel):
+#     height = image.shape[0]
+#     width = image.shape[1]
+#     channels = image.shape[2]
+#
+#     k = kernel.shape[0]//2
+#
+#     empty_arr = np.zeros_like(image)
+#     padded_image = np.pad(image, ((k,k),(k,k),(0,0)), mode='edge')
+#
+#     for i in range(height):
+#         for j in range(width):
+#             for c in range(channels):
+#                 grid = padded_image[i:i+3, j:j+3, c]
+#                 convolution = np.sum(grid*kernel)
+#                 empty_arr[i,j,c] = np.clip(convolution,0,255)
+#     return empty_arr
+
+
+
+
+
+
+
+from PIL import  Image
 import numpy as np
-from PIL import Image
 
-sharpening_kernel = [
-    [-1,-1,-1],
-    [-1,9,-1],
-    [-1,-1,-1]
-]
-sharpening_kernel = np.array(sharpening_kernel)
+sharpening_kernel = np.array ([
+    [-1, -1, -1],
+    [-1, 9, -1],
+    [-1, -1, -1]
+])
 
-def sharpen_image(image,kernel):
+def sharpen_the_image(image, kernel):
     height = image.shape[0]
     width = image.shape[1]
     channels = image.shape[2]
 
-    k = kernel.shape[0]//2
+    k = kernel.shape[0] // 2 #offset from the center
 
-    empty_arr = np.zeros_like(image)
-    padded_image = np.pad(image, ((k,k),(k,k),(0,0)), mode='edge')
+    sharpened_image = np.zeros_like(image)
+    padding = np.pad(image, ((k, k), (k, k), (0, 0)), mode='edge')
 
-    for i in range(height):
-        for j in range(width):
+    for i in range(k, height+k):
+        for j in range(k, width+k):
             for c in range(channels):
-                grid = padded_image[i:i+3, j:j+3, c]
-                convolution = np.sum(grid*kernel)
-                empty_arr[i,j,c] = np.clip(convolution,0,255)
-    return empty_arr
-
+                matrix = padding[i-1:i+2, j-1:j+2, c]
+                value = np.sum(matrix * kernel)
+                sharpened_image[i-k, j-k, c] = np.clip(value, 0, 255)
+    return sharpened_image.astype(np.uint8)
 
 
 
